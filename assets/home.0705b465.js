6fa08c83f2cc36f95588a48b2205fd6bdf7f0210
ljppp <li.jiapeng@wuta-camera.com>
2022-11-03 17:21:54 +0800

    test: actions

--- assets/home.0705b465.js
+++
@@ -1,1 +0,0 @@
-import{g as a,c,a as e,b as s,o as n,r,h as _}from"./app.10e153a7.js";const m={},i={class:"px-4 py-10 text-center text-gray-700 dark:text-gray-200"},p=s("div",{class:"mt-5 mx-auto text-center opacity-75 dark:opacity-50 text-sm"}," [Home Layout] ",-1);function d(x,f){const t=r("RouterView"),o=_;return n(),c("main",i,[e(t),e(o),p])}const u=a(m,[["render",d]]);export{u as default};
