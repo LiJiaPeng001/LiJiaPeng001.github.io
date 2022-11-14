417ad6b61ce9a7899af04cf4916da8c10d86138a
ljppp <li.jiapeng@wuta-camera.com>
2022-11-14 17:12:53 +0800

    feat: new md

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,37 +1,38 @@
 export default {
-  title: '李小朋的博客',
-  description: 'Just a blog',
+  title: "李小朋的博客",
+  description: "Just a blog",
   markdown: {
-    lineNumbers: true
+    lineNumbers: true,
   },
   themeConfig: {
     sidebar: [
       {
-        text: '关于',
-        items: [
-          { text: 'about', link: '/about.md' },
-        ]
+        text: "关于",
+        items: [{ text: "about", link: "/about.md" }],
       },
-      // {
-      //   text: 'Html',
-      //   items: [
-      //     { text: 'index', link: '/html/index.md' },
-      //   ]
-      // },
       {
-        text: 'CSS',
+        text: "Babel插件制作流程",
         items: [
-          { text: 'overflow的visible和hidden', link: '/css/overflow.md' },
-        ]
+          { text: "什么是babel", link: "/babel/babel1.md" },
+          { text: "babel编译流程", link: "/babel/babel2.md" },
+          { text: "babel的AST", link: "/babel/babel3.md" },
+        ],
+      },
+      {
+        text: "CSS",
+        items: [
+          { text: "overflow的visible和hidden", link: "/css/overflow.md" },
+        ],
       },
     ],
     footer: {
-      message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2022-present peng'
+      message: "Released under the MIT License.",
+      copyright: "Copyright © 2022-present peng",
     },
     editLink: {
-      pattern: 'https://github.com/LiJiaPeng001/LiJiaPeng001.github.io/tree/master/docs/:path',
-      text: '修改本页面'
-    }
-  }
-}
+      pattern:
+        "https://github.com/LiJiaPeng001/LiJiaPeng001.github.io/tree/master/docs/:path",
+      text: "修改本页面",
+    },
+  },
+};
